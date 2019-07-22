4dba0e69fec41181fa9f4ba1a186377f16a1924c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2019-07-22 08:36:25 +0530

    3rd version

--- BDDAutomation/src/main/java/com/cucumber/automation/BDDAutomation/App.java
+++ BDDAutomation/src/main/java/com/cucumber/automation/BDDAutomation/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello  New world" );
+        System.out.println( "Hello  New worldyyyy" );
     }
 }
